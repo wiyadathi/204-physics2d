9dd1e2e9250eb1b112da746561d9f131cb931bcc
wiyadathi <wiyada.t@bu.ac.th>
2024-03-31 13:55:09 +0700

    Move with MovePosition and Velocity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,15 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private Vector2 move;
+
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float torque;
     
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        moveSpeed = 1000f;
-        Debug.Log($"speed = {moveSpeed}");
+        moveSpeed = 10f;
     }
 
     // Update is called once per frame
@@ -22,13 +23,23 @@ public class PlayerController : MonoBehaviour
     {
         //get Input from KB
         move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Debug.Log("move..");
+
+        //rotate using AddTorque
+        if (Input.GetKeyDown(KeyCode.R))
+            rb2d.AddTorque(torque);
     }
 
     private void FixedUpdate()
     {
-        //rigidbody movement
+        //move with addforce
         rb2d.AddForce(move * moveSpeed * Time.deltaTime);
-        Debug.Log("add force");
+        
+        //move with MovePosition
+        //rb2d.MovePosition(rb2d.position + (move * moveSpeed * Time.deltaTime));
+        
+        //move with velocity
+        //rb2d.velocity = move * moveSpeed * Time.deltaTime;
     }
 }
+
+
